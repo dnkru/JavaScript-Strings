520c001d1fcfb86b321a26ef55bc27f52b998813
Kevin Kruelle <kkruelle2@gmail.com>
2021-02-02 15:11:40 -0500

    update/finished activity 1 for JavaScript-Strings

--- basics-1.js
+++ basics-1.js
@@ -2,5 +2,7 @@ const userText = prompt('Enter some text');
 const charAmount = `You entered ${userText.length} characters`
 
 console.log(charAmount);
+console.log(userText.indexOf('i'));
+console.log(userText.slice(3));
+console.log(userText.charAt(userText.length-1));
 
-console.log(userText.indexOf('i'))
