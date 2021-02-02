aeb6535edfc3819c3ceaae38b965a8bc64e2316e
Kevin Kruelle <kkruelle2@gmail.com>
2021-02-02 16:20:50 -0500

    activity 2 finished

--- js/strings-2.js
+++ js/strings-2.js
@@ -1,7 +1,13 @@
+/*
 console.log('conected...')
 
 const userName = prompt('Enter name');
-
 const greeting = 'Greeting';
 
 document.getElementById('output').textContent = greeting + userName;
+*/
+
+const userText = prompt('Enter some text.');
+const userTextReplaced = userText.replaceAll('a', '@');
+document.getElementById('output').textContent = userTextReplaced
+
