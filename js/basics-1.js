f811608368cc3d867edc25851f7de1112b4c3ada
Kevin Kruelle <kkruelle2@gmail.com>
2021-02-02 15:38:19 -0500

    updated + added more html/js files

---
+++ js/basics-1.js
@@ -0,0 +1,8 @@
+const userText = prompt('Enter some text');
+const charAmount = `You entered ${userText.length} characters`
+
+console.log(charAmount);
+console.log(userText.indexOf('i'));
+console.log(userText.slice(3));
+console.log(userText.charAt(userText.length-1));
+
